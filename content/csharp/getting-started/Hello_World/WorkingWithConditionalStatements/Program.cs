58f390c8fe6460c266bbe412c8c347e01019bfe6
Gregory Otokhina <gregoryotokhina@gmail.com>
2023-03-28 15:57:23 +0100

    Working with Conditional Statements

---
+++ content/csharp/getting-started/Hello_World/WorkingWithConditionalStatements/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rand = new Random();
+int secretNumber = rand.Next(1, 4); // Generate a random number between 1 and 3
+Console.Write("Guess a number between 1 and 3: ");
+string input = Console.ReadLine();
+int guess;
+if (!int.TryParse(input, out guess)) {
+    Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
+} else if (guess is < 1 or > 3) {
+    Console.WriteLine("Your guess is outside the range of valid numbers. Please enter a number between 1 and 3.");
+} else if (guess == secretNumber) {
+    Console.WriteLine("Congratulations, you guessed the secret number!");
+} else if (guess < secretNumber) {
+    Console.WriteLine("Your guess is too low.");
+} else {
+    Console.WriteLine("Your guess is too high.");
+}
