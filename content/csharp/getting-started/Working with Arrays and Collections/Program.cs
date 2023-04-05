4aea511dbe9c7562c00fdda4f5e3d2caf2a66201
Gregory Otokhina <gregoryotokhina@gmail.com>
2023-04-05 22:44:36 +0100

    doing some tests on arrays

--- content/csharp/getting-started/Working with Arrays and Collections/Program.cs
+++ content/csharp/getting-started/Working with Arrays and Collections/Program.cs
@@ -15,4 +15,28 @@ Console.WriteLine(otherIntegers[1]);
 
         int[,] someTable = { { 1, 2, 3 }, { 4, 5, 6 } }; // you can fill a multi-dimensional array on assignment as well
         Console.WriteLine(someTable[0,0]); // one "corner" of the array
+
+// JAGGED ARRAYS
+int[][] jaggedArray = new int[4][]; // define first dimension
+jaggedArray[0] = new int[2] { 1, 2 }; // set values of first array
+Console.WriteLine(jaggedArray[0][0]); // first element in first row
+Console.WriteLine(jaggedArray[0][1]); // second element in first row
+
+// additional rows haven't yet been created/assigned
+Console.WriteLine(jaggedArray[1]); // value is null
+
+//WORKING WITH ARRAYS
+int[] someNumbers = { 1, 2, 3 };
+
+int x = 1 + someNumbers[0]; // x = 2
+int y = 2 * someNumbers[2]; // y = 6
+someNumbers[2] = y; // someIntegers now contains { 1, 2, 6 }
+// someIntegers[3] = 3; // EXCEPTION (IndexOutOfRange)
+
+//ARRAYS AND STRINGS
+string input = "red,blue,yellow,green";
+string[] colors = input.Split(','); // note single quotes, which are used to define literal character (``char``) values
+
+Console.WriteLine(colors[0]);
+Console.WriteLine(colors[1]);
     
