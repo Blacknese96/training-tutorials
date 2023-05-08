783cf9d8211b57f567727dcae9feb3a3a33d6941
Gregory Otokhina <gregoryotokhina@gmail.com>
2023-05-08 02:28:34 +0100

    Understanding LINQ

---
+++ content/csharp/getting-started/Encapsulation and Object-Oriented Design/Program.cs
@@ -0,0 +1,7 @@
+﻿//ENCAPSULATION
+/*What is Encapsulation? Encapsulation in simple terms refers to the key principle in Object Oriented Design
+thal allows you to control access to an object's data and methods.
+ This way it helps to prevent bugs and errors in your code. It basically helps your code to be more understandable
+ and loosely coupled. */
+ 
+ 
